0b80f50fe44a743007365031ce5aa12c316fbfce
Dmitry Baryshnikov <bishop.dev@gmail.com>
2015-06-28 22:33:56 +0300

    Application skeleton

---
+++ app/src/main/java/com/nextgis/forestinspector/MainApplication.java
@@ -0,0 +1,121 @@
+/*
+ * Project: Forest violations
+ * Purpose: Mobile application for registering facts of the forest violations.
+ * Author:  Dmitry Baryshnikov (aka Bishop), bishop.dev@gmail.com
+ * *****************************************************************************
+ * Copyright (c) 2015-2015. NextGIS, info@nextgis.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.nextgis.forestinspector;
+
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.preference.PreferenceManager;
+
+import com.nextgis.maplib.map.MapBase;
+import com.nextgis.maplib.map.MapDrawable;
+import com.nextgis.maplib.util.SettingsConstants;
+import com.nextgis.maplibui.GISApplication;
+import com.nextgis.maplibui.mapui.LayerFactoryUI;
+import com.nextgis.maplibui.mapui.RemoteTMSLayerUI;
+import com.nextgis.maplibui.util.SettingsConstantsUI;
+
+import java.io.File;
+
+import static com.nextgis.maplib.util.Constants.MAP_EXT;
+import static com.nextgis.maplib.util.GeoConstants.TMSTYPE_OSM;
+import static com.nextgis.maplib.util.SettingsConstants.KEY_PREF_MAP;
+
+public class MainApplication extends GISApplication {
+
+    @Override
+    public MapBase getMap() {
+        if (null != mMap) {
+            return mMap;
+        }
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        File defaultPath = getExternalFilesDir(KEY_PREF_MAP);
+        if (defaultPath == null) {
+            defaultPath = new File(getFilesDir(), KEY_PREF_MAP);
+        }
+
+        String mapPath = sharedPreferences.getString(SettingsConstants.KEY_PREF_MAP_PATH, defaultPath.getPath());
+        String mapName = sharedPreferences.getString(SettingsConstantsUI.KEY_PREF_MAP_NAME, "default");
+
+        File mapFullPath = new File(mapPath, mapName + MAP_EXT);
+
+        final Bitmap bkBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.bg);
+        mMap = new MapDrawable(bkBitmap, this, mapFullPath, new LayerFactoryUI());
+        mMap.setName(mapName);
+        mMap.load();
+
+        return mMap;
+    }
+
+    /**
+     * @return A authority for sync purposes or empty string in not sync anything
+     */
+    @Override
+    public String getAuthority() {
+        return null;
+    }
+
+    /**
+     * Show settings Activity
+     */
+    @Override
+    public void showSettings() {
+
+    }
+
+    @Override
+    protected void onFirstRun()
+    {
+        //add OpenStreetMap layer on application first run
+        String layerName = getString(R.string.osm);
+        String layerURL = getString(R.string.osm_url);
+        RemoteTMSLayerUI osmLayer =
+                new RemoteTMSLayerUI(getApplicationContext(), mMap.createLayerStorage());
+        osmLayer.setName(layerName);
+        osmLayer.setURL(layerURL);
+        osmLayer.setTMSType(TMSTYPE_OSM);
+        osmLayer.setMaxZoom(22);
+        osmLayer.setMinZoom(12.4f);
+        osmLayer.setVisible(true);
+
+        mMap.addLayer(osmLayer);
+        mMap.moveLayer(0, osmLayer);
+
+        String kosmosnimkiLayerName = getString(R.string.topo);
+        String kosmosnimkiLayerURL = getString(R.string.topo_url);
+        RemoteTMSLayerUI ksLayer =
+                new RemoteTMSLayerUI(getApplicationContext(), mMap.createLayerStorage());
+        ksLayer.setName(kosmosnimkiLayerName);
+        ksLayer.setURL(kosmosnimkiLayerURL);
+        ksLayer.setTMSType(TMSTYPE_OSM);
+        ksLayer.setMaxZoom(12.4f);
+        ksLayer.setMinZoom(0);
+        ksLayer.setVisible(true);
+
+        mMap.addLayer(ksLayer);
+        mMap.moveLayer(1, ksLayer);
+
+        mMap.save();
+    }
+}
+
