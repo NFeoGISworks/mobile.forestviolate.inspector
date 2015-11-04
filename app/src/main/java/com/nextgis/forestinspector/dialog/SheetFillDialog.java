c786493fecd2f97c3741c85ced7f5e58ededc56f
NikitaFeodonit <nikitafeodonit@yandex.com>
2015-11-04 12:21:22 +0300

    Tree sheet as a part of the act. Write coordinates (#23)

--- app/src/main/java/com/nextgis/forestinspector/dialog/SheetFillDialog.java
+++ app/src/main/java/com/nextgis/forestinspector/dialog/SheetFillDialog.java
@@ -23,27 +23,44 @@
 package com.nextgis.forestinspector.dialog;
 
 import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.AppCompatSpinner;
 import android.text.InputFilter;
 import android.text.Spanned;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.DaveKoelle.AlphanumComparator;
 import com.nextgis.forestinspector.MainApplication;
 import com.nextgis.forestinspector.R;
 import com.nextgis.forestinspector.activity.SheetActivity;
 import com.nextgis.forestinspector.map.DocumentsLayer;
 import com.nextgis.forestinspector.util.Constants;
+import com.nextgis.maplib.api.GpsEventListener;
 import com.nextgis.maplib.api.ILayer;
 import com.nextgis.maplib.datasource.Feature;
+import com.nextgis.maplib.datasource.GeoMultiPoint;
+import com.nextgis.maplib.datasource.GeoPoint;
+import com.nextgis.maplib.location.GpsEventSource;
 import com.nextgis.maplib.map.MapBase;
 import com.nextgis.maplib.map.NGWLookupTable;
 import com.nextgis.maplib.map.VectorLayer;
+import com.nextgis.maplib.util.GeoConstants;
+import com.nextgis.maplib.util.LocationUtil;
+import com.nextgis.maplibui.util.SettingsConstantsUI;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -52,6 +69,7 @@ import java.util.Map;
 
 public class SheetFillDialog
         extends YesNoDialog
+        implements GpsEventListener
 {
     protected AppCompatSpinner mUnit;
     protected AppCompatSpinner mSpecies;
@@ -66,7 +84,15 @@ public class SheetFillDialog
     protected ArrayAdapter<String> mThicknessAdapter;
     protected ArrayAdapter<String> mHeightAdapter;
 
-    protected DocumentsLayer      mDocsLayer;
+    protected TextView mLatView;
+    protected TextView mLongView;
+    protected TextView mAltView;
+    protected TextView mAccView;
+    protected Location mLocation;
+
+    protected GpsEventSource gpsEventSource;
+
+    protected DocumentsLayer mDocsLayer;
 
     protected OnAddTreesListener mOnAddTreesListener;
 
@@ -135,7 +161,13 @@ public class SheetFillDialog
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState)
     {
+        MainApplication app = (MainApplication) getActivity().getApplication();
+        gpsEventSource = app.getGpsEventSource();
+        gpsEventSource.addListener(this);
+
         View view = View.inflate(getActivity(), R.layout.dialog_sheet_fill, null);
+
+        createLocationPanelView(view);
 
         mUnit = (AppCompatSpinner) view.findViewById(R.id.unit);
         mUnit.setAdapter(mUnitAdapter);
@@ -190,8 +222,133 @@ public class SheetFillDialog
     }
 
 
+    @Override
+    public void onDestroyView()
+    {
+        gpsEventSource.removeListener(this);
+        super.onDestroyView();
+    }
+
+
+    protected void createLocationPanelView(View view)
+    {
+        mLatView = (TextView) view.findViewById(com.nextgis.maplibui.R.id.latitude_view);
+        mLongView = (TextView) view.findViewById(com.nextgis.maplibui.R.id.longitude_view);
+        mAltView = (TextView) view.findViewById(com.nextgis.maplibui.R.id.altitude_view);
+        mAccView = (TextView) view.findViewById(com.nextgis.maplibui.R.id.accuracy_view);
+
+        FrameLayout accLocPanel =
+                (FrameLayout) view.findViewById(com.nextgis.maplibui.R.id.accurate_location_panel);
+        accLocPanel.setVisibility(View.GONE);
+
+        final ImageButton refreshLocation =
+                (ImageButton) view.findViewById(com.nextgis.maplibui.R.id.refresh);
+
+        refreshLocation.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View view)
+                    {
+                        RotateAnimation rotateAnimation = new RotateAnimation(
+                                0, 360, Animation.RELATIVE_TO_SELF, 0.5f,
+                                Animation.RELATIVE_TO_SELF, 0.5f);
+                        rotateAnimation.setDuration(700);
+                        rotateAnimation.setRepeatCount(0);
+                        refreshLocation.startAnimation(rotateAnimation);
+
+                        Location location = gpsEventSource.getLastKnownLocation();
+                        setLocationText(location);
+                    }
+                });
+
+        setLocationText(gpsEventSource.getLastKnownLocation());
+    }
+
+
+    protected void setLocationText(Location location)
+    {
+        if (null == mLatView || null == mLongView || null == mAccView || null == mAltView) {
+            return;
+        }
+
+        if (null == location) {
+
+            mLatView.setText(
+                    getString(com.nextgis.maplibui.R.string.latitude_caption_short) + ": " +
+                    getString(com.nextgis.maplibui.R.string.n_a));
+            mLongView.setText(
+                    getString(com.nextgis.maplibui.R.string.longitude_caption_short) + ": " +
+                    getString(com.nextgis.maplibui.R.string.n_a));
+            mAltView.setText(
+                    getString(com.nextgis.maplibui.R.string.altitude_caption_short) + ": " +
+                    getString(com.nextgis.maplibui.R.string.n_a));
+            mAccView.setText(
+                    getString(com.nextgis.maplibui.R.string.accuracy_caption_short) + ": " +
+                    getString(com.nextgis.maplibui.R.string.n_a));
+
+            return;
+        }
+
+        mLocation = location;
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        int nFormat = prefs.getInt(
+                SettingsConstantsUI.KEY_PREF_COORD_FORMAT + "_int", Location.FORMAT_SECONDS);
+        DecimalFormat df = new DecimalFormat("0.0");
+
+        mLatView.setText(
+                getString(com.nextgis.maplibui.R.string.latitude_caption_short) + ": " +
+                LocationUtil.formatLatitude(location.getLatitude(), nFormat, getResources()));
+
+        mLongView.setText(
+                getString(com.nextgis.maplibui.R.string.longitude_caption_short) + ": " +
+                LocationUtil.formatLongitude(location.getLongitude(), nFormat, getResources()));
+
+        double altitude = location.getAltitude();
+        mAltView.setText(
+                getString(com.nextgis.maplibui.R.string.altitude_caption_short) + ": " +
+                df.format(altitude) + " " +
+                getString(com.nextgis.maplibui.R.string.unit_meter));
+
+        float accuracy = location.getAccuracy();
+        mAccView.setText(
+                getString(com.nextgis.maplibui.R.string.accuracy_caption_short) + ": " +
+                df.format(accuracy) + " " +
+                getString(com.nextgis.maplibui.R.string.unit_meter));
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+
+    }
+
+
+    @Override
+    public void onBestLocationChanged(Location location)
+    {
+
+    }
+
+
+    @Override
+    public void onGpsStatusChanged(int event)
+    {
+
+    }
+
+
     public void addTrees()
     {
+        if (null == mLocation) {
+            Toast.makeText(getActivity(), getString(R.string.error_no_location), Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
         SheetActivity activity = (SheetActivity) getActivity();
         if (null == activity) {
             return;
@@ -220,6 +377,12 @@ public class SheetFillDialog
                              ? 1
                              : Integer.parseInt(mCount.getText().toString());
 
+        GeoPoint pt = new GeoPoint(mLocation.getLongitude(), mLocation.getLatitude());
+        pt.setCRS(GeoConstants.CRS_WGS84);
+        pt.project(GeoConstants.CRS_WEB_MERCATOR);
+        GeoMultiPoint geometryValue = new GeoMultiPoint();
+        geometryValue.add(pt);
+
 
         Feature feature = new Feature(
                 com.nextgis.maplib.util.Constants.NOT_FOUND, sheetLayer.getFields());
@@ -235,6 +398,7 @@ public class SheetFillDialog
                 Constants.FIELD_SHEET_HEIGHTS, mHeight.getSelectedItem().toString());
         feature.setFieldValue(
                 Constants.FIELD_SHEET_COUNT, countValue);
+        feature.setGeometry(geometryValue);
 
         activity.getFeature().addSubFeature(Constants.KEY_LAYER_SHEET, feature);
     }
