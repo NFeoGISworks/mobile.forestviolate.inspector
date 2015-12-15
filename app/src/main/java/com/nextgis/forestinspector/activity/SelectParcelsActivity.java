8c25a0bb3bb6c2f667bc53f055500a693851ee75
NikitaFeodonit <nikitafeodonit@yandex.com>
2015-12-15 12:42:01 +0300

    Fix crash (fix #62)

--- app/src/main/java/com/nextgis/forestinspector/activity/SelectParcelsActivity.java
+++ app/src/main/java/com/nextgis/forestinspector/activity/SelectParcelsActivity.java
@@ -51,27 +51,29 @@ import com.nextgis.forestinspector.datasource.DocumentEditFeature;
 import com.nextgis.forestinspector.util.Constants;
 import com.nextgis.forestinspector.util.SettingsConstants;
 
-/**
- * Created by bishop on 03.08.15.
- */
-public class SelectParcelsActivity extends FIActivity implements LoaderManager.LoaderCallbacks<Cursor> {
-    protected ListView mListView;
+
+public class SelectParcelsActivity
+        extends FIActivity
+        implements LoaderManager.LoaderCallbacks<Cursor>
+{
+    protected ListView            mListView;
     protected ParcelCursorAdapter mAdapter;
     protected DocumentEditFeature mDocumentFeature;
-    protected boolean mFiltered = false;
+    protected boolean mFiltered     = false;
     protected boolean mLoaderIsInit = false;
 
     protected static String KEY_QUERY = "query";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_select_territory_form_cadastre);
         setToolbar(R.id.main_toolbar);
 
-        mListView = (ListView)findViewById(R.id.parcelsList);
+        mListView = (ListView) findViewById(R.id.parcelsList);
         mAdapter = new ParcelCursorAdapter(this, null, 0);
         mListView.setAdapter(mAdapter);
 
@@ -81,37 +83,37 @@ public class SelectParcelsActivity extends FIActivity implements LoaderManager.L
         mDocumentFeature = app.getTempFeature();
     }
 
+
     @Override
-    protected void onNewIntent(Intent intent) {
+    protected void onNewIntent(Intent intent)
+    {
         super.onNewIntent(intent);
 
         handleIntent(intent);
     }
 
-    private void handleIntent(Intent intent) {
 
+    private void handleIntent(Intent intent)
+    {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
             //use the query to search your data
             String fullQuery = Constants.FIELD_CADASTRE_LV + " LIKE '%" + query + "%' OR " +
-                    Constants.FIELD_CADASTRE_ULV + " LIKE '%" + query + "%' OR " +
-                    Constants.FIELD_CADASTRE_PARCEL + " LIKE '%" + query + "%'";
+                               Constants.FIELD_CADASTRE_ULV + " LIKE '%" + query + "%' OR " +
+                               Constants.FIELD_CADASTRE_PARCEL + " LIKE '%" + query + "%'";
 
             Bundle bundle = new Bundle();
             bundle.putString(KEY_QUERY, fullQuery);
-            if(mLoaderIsInit) {
+            if (mLoaderIsInit) {
                 getSupportLoaderManager().restartLoader(0, bundle, this);
-            }
-            else {
+            } else {
                 getSupportLoaderManager().initLoader(0, bundle, this);
                 mLoaderIsInit = true;
             }
-        }
-        else{
-            if(mLoaderIsInit) {
+        } else {
+            if (mLoaderIsInit) {
                 getSupportLoaderManager().restartLoader(0, null, this);
-            }
-            else {
+            } else {
                 getSupportLoaderManager().initLoader(0, null, this);
                 mLoaderIsInit = true;
             }
@@ -120,38 +122,52 @@ public class SelectParcelsActivity extends FIActivity implements LoaderManager.L
 
 
     @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+    public Loader<Cursor> onCreateLoader(
+            int id,
+            Bundle args)
+    {
         Uri uri = Uri.parse(
                 "content://" + SettingsConstants.AUTHORITY + "/" + Constants.KEY_LAYER_KV);
-        if(args == null) {
+        if (args == null) {
             return new CursorLoader(this, uri, null, null, null, null);
-        }
-        else {
+        } else {
             return new CursorLoader(this, uri, null, " " + args.getString(KEY_QUERY), null, null);
         }
     }
 
+
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        if(null != data)
-            mAdapter.swapCursor(data);
+    public void onLoadFinished(
+            Loader<Cursor> loader,
+            Cursor data)
+    {
+        if (null != data) {
+            Cursor oldCursor = mAdapter.swapCursor(data);
+            if (null != oldCursor) {
+                oldCursor.close();
+            }
+        }
     }
 
+
     @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
+    public void onLoaderReset(Loader<Cursor> loader)
+    {
 
     }
 
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.parcels_select, menu);
         MenuItem item = menu.findItem(R.id.action_search);
 
-        if(null != item) {
+        if (null != item) {
             SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
             ParcelSearchView searchView = (ParcelSearchView) MenuItemCompat.getActionView(item);
-            if(null == searchView) {
+            if (null == searchView) {
                 searchView = new ParcelSearchView(getSupportActionBar().getThemedContext());
                 MenuItemCompat.setActionView(item, searchView);
             }
@@ -160,29 +176,28 @@ public class SelectParcelsActivity extends FIActivity implements LoaderManager.L
         return true;
     }
 
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if(id == R.id.action_filter){
-            if(!mFiltered){
+        if (id == R.id.action_filter) {
+            if (!mFiltered) {
                 String fullQuery = mDocumentFeature.getWhereClauseForParcelIds();
                 Bundle bundle = new Bundle();
                 bundle.putString(KEY_QUERY, fullQuery);
                 getSupportLoaderManager().restartLoader(0, bundle, this);
-            }
-
-            else{
+            } else {
                 getSupportLoaderManager().restartLoader(0, null, this);
             }
             mFiltered = !mFiltered;
 
             return true;
-        }
-        else if (id == R.id.action_apply) {
+        } else if (id == R.id.action_apply) {
             apply();
             return true;
         }
@@ -190,88 +205,139 @@ public class SelectParcelsActivity extends FIActivity implements LoaderManager.L
         return super.onOptionsItemSelected(item);
     }
 
-    private void apply() {
+
+    private void apply()
+    {
         finish();
     }
 
-    protected class ParcelCursorAdapter extends CursorAdapter {
-        public ParcelCursorAdapter(Context context, Cursor cursor, int flags) {
+
+    protected class ParcelCursorAdapter
+            extends CursorAdapter
+    {
+        public ParcelCursorAdapter(
+                Context context,
+                Cursor cursor,
+                int flags)
+        {
             super(context, cursor, 0);
         }
+
 
         // The newView method is used to inflate a new view and return it,
         // you don't bind any data to the view at this point.
         @Override
-        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        public View newView(
+                Context context,
+                Cursor cursor,
+                ViewGroup parent)
+        {
             return LayoutInflater.from(context).inflate(R.layout.item_parcel, parent, false);
         }
+
 
         // The bindView method is used to bind all data to a given view
         // such as setting the text on a TextView.
         @Override
-        public void bindView(View view, Context context, Cursor cursor) {
+        public void bindView(
+                View view,
+                Context context,
+                Cursor cursor)
+        {
             // Find fields to populate in inflated template
             TextView tvParcel = (TextView) view.findViewById(R.id.parcel_desc);
 
-            String sParcelDesc = cursor.getString(cursor.getColumnIndexOrThrow(Constants.FIELD_CADASTRE_LV)) +
+            String sParcelDesc =
+                    cursor.getString(cursor.getColumnIndexOrThrow(Constants.FIELD_CADASTRE_LV)) +
                     " " + getString(R.string.forestry) + ", " +
                     cursor.getString(cursor.getColumnIndexOrThrow(Constants.FIELD_CADASTRE_ULV)) +
-                    " " + getString(R.string.district_forestry) + ", " + getString(R.string.parcel) + " " +
+                    " " + getString(R.string.district_forestry) + ", " +
+                    getString(R.string.parcel) + " " +
                     cursor.getString(cursor.getColumnIndexOrThrow(Constants.FIELD_CADASTRE_PARCEL));
-            long id = cursor.getLong(cursor.getColumnIndexOrThrow(com.nextgis.maplib.util.Constants.FIELD_ID));
+            long id = cursor.getLong(
+                    cursor.getColumnIndexOrThrow(com.nextgis.maplib.util.Constants.FIELD_ID));
             // Populate fields with extracted properties
             tvParcel.setText(sParcelDesc);
 
             CheckBox box = (CheckBox) view.findViewById(R.id.check);
             box.setTag(id);
-            if(mDocumentFeature.getParcelIds().contains(id))
+            if (mDocumentFeature.getParcelIds().contains(id)) {
                 box.setChecked(true);
-            else
+            } else {
                 box.setChecked(false);
-            box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    Long id = (Long) buttonView.getTag();
-                    if(isChecked) {
-                        if(!mDocumentFeature.getParcelIds().contains(id))
-                            mDocumentFeature.getParcelIds().add(id);
-                    }
-                    else
-                        mDocumentFeature.getParcelIds().remove(id);
-                }
-            });
+            }
+            box.setOnCheckedChangeListener(
+                    new CompoundButton.OnCheckedChangeListener()
+                    {
+                        @Override
+                        public void onCheckedChanged(
+                                CompoundButton buttonView,
+                                boolean isChecked)
+                        {
+                            Long id = (Long) buttonView.getTag();
+                            if (isChecked) {
+                                if (!mDocumentFeature.getParcelIds().contains(id)) {
+                                    mDocumentFeature.getParcelIds().add(id);
+                                }
+                            } else {
+                                mDocumentFeature.getParcelIds().remove(id);
+                            }
+                        }
+                    });
         }
     }
 
-    public class ParcelSearchView extends SearchView {
 
-        public ParcelSearchView(Context context) {
+    public class ParcelSearchView
+            extends SearchView
+    {
+
+        public ParcelSearchView(Context context)
+        {
             super(context);
             init();
         }
 
-        public ParcelSearchView(Context context, AttributeSet attrs) {
+
+        public ParcelSearchView(
+                Context context,
+                AttributeSet attrs)
+        {
             super(context, attrs);
             init();
         }
 
-        public ParcelSearchView(Context context, AttributeSet attrs, int defStyleAttr) {
+
+        public ParcelSearchView(
+                Context context,
+                AttributeSet attrs,
+                int defStyleAttr)
+        {
             super(context, attrs, defStyleAttr);
             init();
         }
 
-        private void init(){
-            ImageView closeButton = (ImageView) findViewById(android.support.v7.appcompat.R.id.search_close_btn);
-            closeButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    getSupportLoaderManager().restartLoader(0, null, SelectParcelsActivity.this);
-                }
-            });
+
+        private void init()
+        {
+            ImageView closeButton =
+                    (ImageView) findViewById(android.support.v7.appcompat.R.id.search_close_btn);
+            closeButton.setOnClickListener(
+                    new View.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(View v)
+                        {
+                            getSupportLoaderManager().restartLoader(
+                                    0, null, SelectParcelsActivity.this);
+                        }
+                    });
         }
 
+
         @Override
-        public void onActionViewCollapsed() {
+        public void onActionViewCollapsed()
+        {
             super.onActionViewCollapsed();
             getSupportLoaderManager().restartLoader(0, null, SelectParcelsActivity.this);
         }
